347e4f522869eb6e82b717c4fdd942ea91175b8c
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2017-03-01 23:56:15 +0600

    Refactor resizing & return back watermarker config

--- app/controllers/neofiles/images_controller.rb
+++ app/controllers/neofiles/images_controller.rb
@@ -54,19 +54,22 @@ class Neofiles::ImagesController < ActionController::Metal
       raise Mongoid::Errors::DocumentNotFound unless width.between?(1, CROP_MAX_WIDTH) and height.between?(1, CROP_MAX_HEIGHT)
     end
 
-    crop_requested = Neofiles.crop_requested? params
-    need_resize_without_crop = width && height && (image_file.width > width || image_file.height > height)
+    crop_requested            = Neofiles.crop_requested? params
+    resizing                  = width && height
+    need_resize_without_crop  = resizing && (image_file.width > width || image_file.height > height)
 
     image.combine_options('convert') do |convert|
-      resize_image(convert, width, height, crop_requested, need_resize_without_crop) if width && height
-      # set watermark
-      width, height = image_file.width, image_file.height if !crop_requested && !need_resize_without_crop
-      add_watermark(convert, image, nowm?(image_file), width, height)
+      resize_image convert, width, height, crop_requested, need_resize_without_crop if resizing
 
-      compress_image(convert, quality) if quality
+      unless nowm?(image_file)
+        width, height = image_file.width, image_file.height if !crop_requested && !need_resize_without_crop
+        add_watermark convert, image, width, height
+      end
+
+      compress_image convert, quality if quality || resizing
     end
 
-    # use pngquant when quality less than 75
+    # use pngquant when quality is less than 75
     ::PngQuantizator::Image.new(image.path).quantize! if options[:type] == 'image/png' && quality && quality < 75
 
     data = image.to_blob
@@ -115,7 +118,7 @@ class Neofiles::ImagesController < ActionController::Metal
   # Fill convert command pipe with compression commands for JPEG and PNG
   # More information: https://www.smashingmagazine.com/2015/06/efficient-image-resizing-with-imagemagick/
   def compress_image(convert, quality)
-    convert.quality "#{quality}"
+    convert.quality "#{quality}" if quality
     convert << '-unsharp' << '0.25x0.25+8+0.065'
     convert << '-dither' << 'None'
     #convert << '-posterize' << '136' # posterize slows down imagamagick extremely in some env due to buggy libgomp1
@@ -127,6 +130,11 @@ class Neofiles::ImagesController < ActionController::Metal
     convert << '-interlace' << 'none'
     convert << '-colorspace' << 'sRGB'
     convert.strip
+  end
+
+  # Add watermarks command to a command pipe, if watermarker is present
+  def add_watermark(convert, image, width, height)
+    Rails.application.config.neofiles.watermarker.try :call, convert, image, width, height
   end
 
 end
