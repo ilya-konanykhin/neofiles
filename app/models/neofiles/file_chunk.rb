f979f871ae850a690cf574a93bfa5cdf6a1e8b0b
desu-ex <maximshirokov21@gmail.com>
2017-06-07 16:06:44 +0500

    добавлен индекс file_id_n_index на files.chunks

--- app/models/neofiles/file_chunk.rb
+++ app/models/neofiles/file_chunk.rb
@@ -12,6 +12,8 @@ class Neofiles::FileChunk
   field :n, type: Integer, default: 0 # что это за поле?
   field :data, type: BSON::Binary
 
+  index({file_id: 1, n: 1}, background: true)
+
   def to_s
     data.data
   end
